838567ae3085c245b42e84d9a1aae55eb3a62806
skythe7 <skythe761@gmail.com>
2026-08-02 09:41:01 +0700

    Add map as location input for report_make

--- static/scripts/report_make.js
+++ static/scripts/report_make.js
@@ -8,3 +8,65 @@ form.addEventListener("submit", () => {
     button.textContent = "Submitting...";
 
 });
+
+const map = L.map("map").setView([-2.5,118],5);
+
+L.tileLayer(
+    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    {
+        attribution:"© OpenStreetMap"
+    }
+).addTo(map);
+
+let marker = null;
+
+const latInput = document.getElementById("latitude");
+const lngInput = document.getElementById("longitude");
+
+const locationText =
+document.getElementById("locationText");
+
+function setLocation(lat,lng){
+
+    latInput.value = lat;
+    lngInput.value = lng;
+
+    locationText.textContent =
+        `${lat.toFixed(6)}, ${lng.toFixed(6)}`;
+
+    if(marker){
+
+        map.removeLayer(marker);
+
+    }
+
+    marker =
+        L.marker([lat,lng]).addTo(map);
+
+}
+
+map.on("click",(e)=>{
+
+    setLocation(
+        e.latlng.lat,
+        e.latlng.lng
+    );
+
+});
+
+document
+.getElementById("myLocation")
+.addEventListener("click",()=>{
+
+    navigator.geolocation.getCurrentPosition(pos=>{
+
+        const lat = pos.coords.latitude;
+        const lng = pos.coords.longitude;
+
+        map.setView([lat,lng],15);
+
+        setLocation(lat,lng);
+
+    });
+
+});
